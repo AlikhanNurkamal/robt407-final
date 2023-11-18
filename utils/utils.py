9d62254dd607117fe8fafda7fb9e582696a876bc
AlikhanNurkamal <nurkamalalikhan@gmail.com>
2023-11-18 14:23:49 +0600

    custom dataset

--- utils/utils.py
+++ utils/utils.py
@@ -7,21 +7,33 @@ from PIL import Image
 from glob import glob
 
 
-class TrainDataset(Dataset):
-    def __init__(self, data_dir, transform=None):
+class MyDataset(Dataset):
+    def __init__(self, root_dir, imgs, labels, transform=None):
         super().__init__()
-        self.data_dir = data_dir
+        self.root_dir = root_dir
         self.transform = transform
-        self.labels = []
 
-        for label in list(glob(os.path.join(data_dir, '*'))):
-            pass
-    
+        # here, imgs and labels are columns of the CSV file
+        self.imgs = imgs
+        self.labels = labels
+
+        # stacking the root path and the image names
+        self.images = [os.path.join(self.root_dir, img) for img in imgs]
+
     def __len__(self):
-        pass
+        return len(self.images)
 
     def __getitem__(self, index):
-        return None, None
+        # getting the path to one image
+        img_path = self.images[index]
+
+        image = np.array(Image.open(img_path).convert('RGB'))
+        label = self.labels[index]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
 
 
 class InferenceDataset(Dataset):
