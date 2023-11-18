7b80be3fb779d7385dab9b2709a18a743d55fa6a
AlikhanNurkamal <nurkamalalikhan@gmail.com>
2023-11-18 14:12:41 +0600

    training code and cnn model

--- models/cnn.py
+++ models/cnn.py
@@ -4,7 +4,9 @@ import torch.nn.functional as F
 
 
 class Block(nn.Module):
-    def __init__(self, in_features, out_features, kernel=3, stride=1, padding=0) -> None:
+    def __init__(self, num_layers, in_features, out_features, kernel=3, stride=1, padding=0):
+        assert num_layers in [18, 34, 50], "unknown architecture"
+        
         super().__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_features, out_features, kernel, stride, padding),
@@ -17,7 +19,7 @@ class Block(nn.Module):
         return x
 
 
-class MyModel(nn.Module):
+class MyResNet(nn.Module):
     def __init__(self, in_features, out_features):
         super().__init__()
         self.conv1 = Block(in_features, 32, kernel=3, stride=1, padding=0)
