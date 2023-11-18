cbc34bc34f57bab1407b56dfd2a0cad79f29e705
AlikhanNurkamal <nurkamalalikhan@gmail.com>
2023-11-18 18:44:01 +0600

    corrected custom dataset

--- utils/utils.py
+++ utils/utils.py
@@ -1,31 +1,31 @@
 import os
 import numpy as np
-import torch
 from torch.utils.data import Dataset
-from torchvision import transforms
 from PIL import Image
 from glob import glob
 
 
 class CustomDataset(Dataset):
-    def __init__(self, root_dir, imgs, labels, transform=None):
+    def __init__(self, images_dir, transform=None):
         super().__init__()
-        self.root_dir = root_dir
+        self.images_dir = images_dir
         self.transform = transform
 
-        # here, imgs and labels are columns of the CSV file
-        self.imgs = imgs
-        self.labels = labels
-
-        # stacking the root path and the image names
-        self.images = [os.path.join(self.root_dir, img) for img in imgs]
+        self.images_paths = []
+        self.labels = []
+        
+        # iterate through each folder (name of a folder corresponds to a class)
+        for label in list(glob(os.path.join(images_dir, '*'))):
+            images = list(glob(os.path.join(images_dir, label, '*.jpg')))
+            self.labels.extend([int(label[-1]) for i in range(len(images))])
+            self.images_paths.extend([image for image in images])
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, index):
         # getting the path to one image
-        img_path = self.images[index]
+        img_path = self.images_paths[index]
 
         image = np.array(Image.open(img_path).convert('RGB'))
         label = self.labels[index]
