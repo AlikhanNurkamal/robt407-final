a3bceaafa7abdcf6935b7bffde38dd9370f97ce2
AlikhanNurkamal <nurkamalalikhan@gmail.com>
2023-11-23 22:37:08 +0600

    added configs

--- config.py
+++ config.py
@@ -10,7 +10,8 @@ config = {
     'BATCH_SIZE': 64,
     'EPOCHS': 100,
     'PATIENCE': 7,
-    'LEARNING_RATE': 1e-4,
+    'LR_INIT': 1e-4,  # pls change this
+    'LR_FINAL': 1e-4,  # pls change this
     'WEIGHT_DECAY': 5e-4,
     'NUM_CLASSES': 10,
     'NUM_WORKERS': 4,
