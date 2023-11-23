8ba5da2f16db76af74b18b902d75d28b2a89d394
AlikhanNurkamal <nurkamalalikhan@gmail.com>
2023-11-23 22:27:18 +0600

    project configs

--- config.py
+++ config.py
@@ -1,2 +1,18 @@
-class Config:
-    pass
+# Description: Configuration file for the project
+# Author: Alikhan Nurkamal, Nail Fakhrutdinov
+# Last Modified: 2023-11-23
+
+# Config variables
+config = {
+    'ROOT_DIR': 'data/',
+    'TEST_DIR': 'data/test/',
+    'IMG_SIZE': 224,
+    'BATCH_SIZE': 64,
+    'EPOCHS': 100,
+    'PATIENCE': 7,
+    'LEARNING_RATE': 0.0001,
+    'NUM_CLASSES': 10,
+    'NUM_WORKERS': 4,
+    'NUM_CHANNELS': 3,
+    'DEVICE': 'cuda',
+}
