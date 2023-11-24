9170c285eb30b50c641ff575baaf052b7c6e642e
AlikhanNurkamal <nurkamalalikhan@gmail.com>
2023-11-24 21:15:26 +0600

    updated configs

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 # Description: Configuration file for the project
 # Author: Alikhan Nurkamal, Nail Fakhrutdinov
 # Last Modified: 2023-11-24
+import torch
 
 # Config variables
 config = {
-    'ROOT_DIR': 'data/',
+    'TRAIN_DIR': 'data/train/',
     'TEST_DIR': 'data/test/',
     'MODELS_DIR': 'models/',
     'IMG_SIZE': 224,
     'BATCH_SIZE': 64,
     'EPOCHS': 100,
-    'PATIENCE': 7,
-    'LR_INIT': 3e-3,  
-    'LR_FINAL': 1e-4,  # pls change this / probably not needed
-    'WEIGHT_DECAY': 5e-4, # too much?
+    'PATIENCE': 9,
+    'LR_INIT': 3e-3,
+    'WEIGHT_DECAY': 5e-4,
     'NUM_CLASSES': 10,
     'NUM_WORKERS': 4,
     'NUM_CHANNELS': 3,
-    'DEVICE': 'cuda',
+    'DEVICE': torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'),
 }
