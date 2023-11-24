614159787673ad21dbcb98e966fa8efa405600f0
Nail Fakhrutdinov <fano2458@gmail.com>
2023-11-24 18:40:04 +0600

    Update vit.py

--- models/vit.py
+++ models/vit.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 
 
 class PatchEmbedding(nn.Module):
-    
     def __init__(self, in_channels: int=3, patch_size: int=16, embedding_dim: int=768):
         super().__init__()
         
@@ -20,8 +19,6 @@ class PatchEmbedding(nn.Module):
         def forward(self, x):
             image_resolution = x.shape[-1]
             
-            assert image_resolution % patch_size == 0, "Image dimentions must be divisible by patch size"
-            
             x = self.patches(x)
             x = self.flatten(x)
             
@@ -30,7 +27,6 @@ class PatchEmbedding(nn.Module):
         
 
 class MSA(nn.Module):
-    
     def __init__(self, embedding_dim: int=768, num_heads: int=12, dropout: float=0):
         super().__init__()
         
@@ -75,4 +71,83 @@ class MLP(nn.Module):
         return x
         
         
+class EncoderBlock(nn.Module):
+    def __init__(self,
+                 embedding_dim: int=768,
+                 num_heads: int=12,
+                 mlp_size: int=3072,
+                 mlp_dropout: float=0.1,
+                 msa_dropout: float=0.0):
+        super().__init__()
+        
+        self.msa = MSA(embedding_dim=embedding_dim,
+                       num_heads=num_heads,
+                       dropout=msa_dropout)
+        
+        self.mlp = MLP(embedding_dim=embedding_dim,
+                       mlp_size=mlp_size,
+                       dropout=mlp_dropout)
+        
+    def forward(self, x):
+        x = self.msa(x) + x
+        x = self.mlp(x) + x
+        
+        return x
+    
+    
+class ViT(nn.Module):
+    def __init__(self,
+                 img_size: int=224,
+                 in_channels: int=3,
+                 patch_size: int=16,
+                 layers: int=12,
+                 embedding_dim: int=768,
+                 mlp_size: int=3072,
+                 num_heads: int=12,
+                 msa_dropout: float=0.0,
+                 mlp_dropout: float=0.1,
+                 emb_dropout: float=0.1,
+                 num_classes: int=10):
+        super().__init__()
+        
+        self.num_patches = (img_size*img_size) // patch_size**2
+        
+        self.class_embedding = nn.Parameter(data=torch.rand(1, 1, embedding_dim), requires_grad=True)
+        
+        self.pos_embedding = nn.Parameter(data=torch.rand(1, self.num_patches+1, embedding_dim), requires_grad=True)
+        
+        self.emb_dropout = nn.Dropout(p=emb_dropout)
+        
+        self.patch_embedding = PatchEmbedding(in_channels=in_channels,
+                                              patch_size=patch_size,
+                                              embedding_dim=embedding_dim)
+        
+        self.encoder = nn.Sequential(*[
+            EncoderBlock(embedding_dim=embedding_dim,
+                         num_heads=num_heads,
+                         mlp_size=mlp_size,
+                         mlp_dropout=mlp_dropout,
+                         msa_dropout=msa_dropout)
+            for _ in range(layers)
+        ])
+        
+        self.head = nn.Sequential(
+            nn.LayerNorm(normalized_shape=embedding_dim),
+            nn.Linear(in_features=embedding_dim,
+                      out_features=num_classes)
+        )
+        
+    def forward(self, x):
+        batch_size = x.shape[0]
+        
+        cls_token = self.class_embedding.expand(batch_size, -1, -1) 
+        
+        x = self.patch_embedding(x)
+        x = torch.cat((cls_token, x), dim=1)
+        x = self.pos_embedding + x
+        x = self.emb_dropout(x)
+        x = self.encoder(x)
+        x = self.head(x[:, 0])
+        
+        return x
         
