b106a496040c220ccdd7947929bf85c4bb6d0689
Nail Fakhrutdinov <fano2458@gmail.com>
2023-11-24 18:12:16 +0600

    Update vit.py

--- models/vit.py
+++ models/vit.py
@@ -28,4 +28,51 @@ class PatchEmbedding(nn.Module):
             return x.permute(0, 2, 1)
         
         
+
+class MSA(nn.Module):
+    
+    def __init__(self, embedding_dim: int=768, num_heads: int=12, dropout: float=0):
+        super().__init__()
+        
+        self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+        
+        self.msa_attention = nn.MultiheadAttention(embed_dim=embedding_dim,
+                                                   num_heads=num_heads,
+                                                   dropout=dropout,
+                                                   batch_first=True)
+        
+        def forward(self, x):
+            x = self.layer_norm(x)
+            x, _ = self.msa_attention(query=x,
+                                      key=x,
+                                      value=x,
+                                      need_weights=False)
+            return x
+        
+        
+class MLP(nn.Module):
+    def __init__(self, 
+                 embedding_dim: int=768,
+                 mlp_size: int=3072,
+                 dropout: float=0.1):
+        super().__init__()
+        
+        self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+        
+        self.mlp = nn.Sequential(
+                nn.Linear(in_features=embedding_dim,
+                        out_features=mlp_size),
+                nn.GELU(),
+                nn.Dropout(p=dropout),
+                nn.Linear(in_features=mlp_size,
+                        out_features=embedding_dim),
+                nn.Dropout(p=dropout))
+        
+    def forward(self, x):
+        x = self.layer_norm(x)
+        x = self.mlp(x)
+        
+        return x
+        
+        
         
