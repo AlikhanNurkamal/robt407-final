2bfd21d1922c3c8ce4223f43635df7ddeea192b9
Nail Fakhrutdinov <fano2458@gmail.com>
2023-11-24 19:06:24 +0600

    update ViT

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Description: Configuration file for the project
 # Author: Alikhan Nurkamal, Nail Fakhrutdinov
-# Last Modified: 2023-11-23
+# Last Modified: 2023-11-24
 
 # Config variables
 config = {
@@ -11,9 +11,9 @@ config = {
     'BATCH_SIZE': 64,
     'EPOCHS': 100,
     'PATIENCE': 7,
-    'LR_INIT': 1e-4,  # pls change this
-    'LR_FINAL': 1e-4,  # pls change this
-    'WEIGHT_DECAY': 5e-4,
+    'LR_INIT': 0.003,  
+    'LR_FINAL': 1e-4,  # pls change this / probably not needed
+    'WEIGHT_DECAY': 0.3, # too much?
     'NUM_CLASSES': 10,
     'NUM_WORKERS': 4,
     'NUM_CHANNELS': 3,
--- models/vit.py
+++ models/vit.py
@@ -3,35 +3,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+# convering image into embeddings
 class PatchEmbedding(nn.Module):
     def __init__(self, in_channels: int=3, patch_size: int=16, embedding_dim: int=768):
         super().__init__()
-        
+        # patches are extracted using convolution, 
         self.patches = nn.Conv2d(in_channels=in_channels,
                                  out_channels=embedding_dim,
                                  kernel_size=patch_size,
-                                 stride=patch_size,
+                                 stride=patch_size, # non overlapping
                                  padding=0)
-        
+        # extracted patches are then flattened into embeddings
         self.flatten = nn.Flatten(start_dim=2, end_dim=3)
         
         
     def forward(self, x):
-        image_resolution = x.shape[-1]
-        
         x = self.patches(x)
         x = self.flatten(x)
         
         return x.permute(0, 2, 1)
         
         
-
+# MultiHeadAttention Module
 class MSA(nn.Module):
     def __init__(self, embedding_dim: int=768, num_heads: int=12, dropout: float=0):
         super().__init__()
-        
+        # layer normalization layer
         self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
-        
+        # multiheadattention layer
         self.msa_attention = nn.MultiheadAttention(embed_dim=embedding_dim,
                                                    num_heads=num_heads,
                                                    dropout=dropout,
@@ -39,28 +38,29 @@ class MSA(nn.Module):
         
     def forward(self, x):
         x = self.layer_norm(x)
+        # query, key, value are formed from the same x
         x, _ = self.msa_attention(query=x,
                                     key=x,
                                     value=x,
                                     need_weights=False)
         return x
     
-        
+# MLP layer   
 class MLP(nn.Module):
     def __init__(self, 
                  embedding_dim: int=768,
                  mlp_size: int=3072,
                  dropout: float=0.1):
         super().__init__()
-        
+        # layer normalization
         self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
-        
+        # two linear layers separated by GeLU activation
         self.mlp = nn.Sequential(
                 nn.Linear(in_features=embedding_dim,
-                        out_features=mlp_size),
+                        out_features=mlp_size), # from input_dim to mlp_size
                 nn.GELU(),
                 nn.Dropout(p=dropout),
-                nn.Linear(in_features=mlp_size,
+                nn.Linear(in_features=mlp_size, # from mlp_size to input_dim
                         out_features=embedding_dim),
                 nn.Dropout(p=dropout))
         
@@ -70,7 +70,7 @@ class MLP(nn.Module):
         
         return x
         
-        
+# Transformer encoder block
 class EncoderBlock(nn.Module):
     def __init__(self,
                  embedding_dim: int=768,
@@ -79,7 +79,7 @@ class EncoderBlock(nn.Module):
                  mlp_dropout: float=0.1,
                  msa_dropout: float=0.0):
         super().__init__()
-        
+        # each block (layer) consists of MSA and MLP
         self.msa = MSA(embedding_dim=embedding_dim,
                        num_heads=num_heads,
                        dropout=msa_dropout)
@@ -89,12 +89,12 @@ class EncoderBlock(nn.Module):
                        dropout=mlp_dropout)
         
     def forward(self, x):
-        x = self.msa(x) + x
-        x = self.mlp(x) + x
+        x = self.msa(x) + x # skip connection #1
+        x = self.mlp(x) + x # skip connection #2
         
         return x
     
-    
+# ViT class
 class ViT(nn.Module):
     def __init__(self,
                  img_size: int=224,
@@ -109,19 +109,19 @@ class ViT(nn.Module):
                  emb_dropout: float=0.1,
                  num_classes: int=1000):
         super().__init__()
-        
+        # calculating number of patches
         self.num_patches = (img_size*img_size) // patch_size**2
-        
+        # creating additional class embedding (token)
         self.class_embedding = nn.Parameter(data=torch.rand(1, 1, embedding_dim), requires_grad=True)
-        
+        # creating learnable positional embeddings
         self.pos_embedding = nn.Parameter(data=torch.rand(1, self.num_patches+1, embedding_dim), requires_grad=True)
         
         self.emb_dropout = nn.Dropout(p=emb_dropout)
-        
+        # image -> embeddings
         self.patch_embedding = PatchEmbedding(in_channels=in_channels,
                                               patch_size=patch_size,
                                               embedding_dim=embedding_dim)
-        
+        # transformer layers
         self.encoder = nn.Sequential(*[
             EncoderBlock(embedding_dim=embedding_dim,
                          num_heads=num_heads,
@@ -130,7 +130,7 @@ class ViT(nn.Module):
                          msa_dropout=msa_dropout)
             for _ in range(layers)
         ])
-        
+        # classification head 
         self.head = nn.Sequential(
             nn.LayerNorm(normalized_shape=embedding_dim),
             nn.Linear(in_features=embedding_dim,
@@ -139,15 +139,15 @@ class ViT(nn.Module):
         
     def forward(self, x):
         batch_size = x.shape[0]
-        
+        # class token
         cls_token = self.class_embedding.expand(batch_size, -1, -1) 
         
-        x = self.patch_embedding(x)
-        x = torch.cat((cls_token, x), dim=1)
-        x = self.pos_embedding + x
+        x = self.patch_embedding(x) # embeddings
+        x = torch.cat((cls_token, x), dim=1) # embeddings + class token
+        x = self.pos_embedding + x # making positional embeddings
         x = self.emb_dropout(x)
-        x = self.encoder(x)
-        x = self.head(x[:, 0])
+        x = self.encoder(x) # transformer layers
+        x = self.head(x[:, 0]) # classification is done based on class embedding (token)
         
         return x
         
